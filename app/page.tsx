0150566b5f29ad44d9bc7916d388911fbe0d0342
Thomas Savournin <tosave.vbl@gmail.com>
2024-04-25 16:23:12 +0200

    feat: label zpl > apercu png > print

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,176 @@
-import Image from "next/image";
+"use client"
+
+import Image from "next/image"
+import { useEffect, useState } from "react"
+
+const DPMM = 8
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+	const [label, setLabel] = useState<string>()
+	const [blob, setBlob] = useState<string>()
+	const [base64Data, setBase64Data] = useState<string>()
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+	// On récupère le labe au format ZPL
+	useEffect(() => {
+		const getLabel = async () => {
+			const label = jszpl()
+			setLabel(label)
+		}
+		getLabel()
+	}, [])
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+	// On envoie le label au service Labelary pour obtenir l'image
+	useEffect(() => {
+		if (!label) return setBlob(undefined)
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+		const getBlob = async () => {
+			const data = await labelary({ zpl: label })
+			setBase64Data(data?.base64Data && `data:image/png;base64,${data?.base64Data}`)
+			setBlob(data?.blob && URL.createObjectURL(data?.blob))
+		}
+		getBlob()
+	}, [label])
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+	const handleOnPrint = async () => printZpl({ zpl: label })
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+	return (
+		<main className="container mx-auto min-h-screen bg-white p-16">
+			<h1 className="text-2xl font-bold text-neutral-700 text-center mb-8">
+				Alpes Profilage
+				<br />
+				Création d&apos;étiquette pour Brother TD-4650
+			</h1>
+
+			<section className="grid grid-cols-4 gap-8">
+				<div className="flex flex-col items-center gap-8 col-span-full border-y py-8">
+					{blob && (
+						<Image className="w-96 border border-neutral-500" src={blob} width={800} height={800} alt="Étiquette" />
+					)}
+
+					<button
+						className="px-4 py-2 border rounded bg-white hover:bg-neutral-100 transition-colors duration-200"
+						onClick={handleOnPrint}
+					>
+						Imprimer
+					</button>
+				</div>
+				{/* <img src={`data:image/png;base64,${base64Data}`} alt="logo"></img> */}
+
+				<div>
+					<h2 className="text-lg font-bold text-neutral-700 mb-4">Code ZPL</h2>
+					<pre className="whitespace-normal break-words">{label}</pre>
+				</div>
+
+				{/* On affiche le contenu du Blob */}
+				<div className="col-span-3">
+					<h2 className="text-lg font-bold text-neutral-700 mb-4">Données en base 64</h2>
+					<pre className="whitespace-normal break-words">{base64Data}</pre>
+				</div>
+			</section>
+		</main>
+	)
+}
+
+// On génère le ZPL
+// https://github.com/DanieLeeuwner/JSZPL/tree/master?tab=readme-ov-file
+const jszpl = () => {
+	const zlp = require("jszpl")
+
+	const NOM = "NOM_DU_CLIENT"
+	const REF = "REF_COMMANDE"
+
+	const label = new zlp.Label()
+	label.printDensity = new zlp.PrintDensity(zlp.PrintDensityName["8dpmm"])
+	label.width = 102
+	label.height = 25
+	label.padding = new zlp.Spacing(20)
+
+	const barcode = new zlp.Barcode()
+	label.content.push(barcode)
+	barcode.type = new zlp.BarcodeType(zlp.BarcodeTypeName.QRCode)
+	barcode.height = new zlp.Size(0.5, zlp.SizeType.Relative)
+	barcode.data = `${NOM};${REF}`
+
+	const grid = new zlp.Grid()
+	label.content.push(grid)
+	grid.columns.push(new zlp.Size(1, zlp.SizeType.Relative))
+	grid.rows.push(new zlp.Size(1, zlp.SizeType.Relative))
+	grid.rows.push(new zlp.Size(1, zlp.SizeType.Relative))
+	grid.left = 26 * DPMM
+	grid.width = 70 * DPMM
+	grid.columnSpacing = 0
+	grid.rowSpacing = 0
+	grid.border = 1
+	grid.padding = new zlp.Spacing(2 * DPMM)
+
+	const text00 = new zlp.Text()
+	grid.content.push(text00)
+	text00.text = NOM
+	text00.text += "\n"
+	text00.text += REF
+	text00.fontFamily = new zlp.FontFamily(zlp.FontFamilyName.D)
+
+	const text10 = new zlp.Text()
+	grid.content.push(text10)
+	text10.text = "(1, 0)"
+	text10.fontFamily = new zlp.FontFamily(zlp.FontFamilyName.D)
+	text10.grid.column = 1
+
+	const text01 = new zlp.Text()
+	grid.content.push(text01)
+	text01.text = "(0, 1)"
+	text01.fontFamily = new zlp.FontFamily(zlp.FontFamilyName.D)
+	text01.grid.row = 1
+
+	const zpl = label.generateZPL()
+
+	return String(zpl)
+}
+
+// On envoie le ZPL au service API Labelary pour obtenir l'image
+// https://labelary.com/service.html
+const labelary = async ({ zpl }: { zpl: string }) => {
+	const url = `http://api.labelary.com/v1/printers/${DPMM}dpmm/labels/4x1/0/`
+
+	const options = {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/x-www-form-urlencoded",
+		},
+		body: zpl,
+	}
+
+	return fetch(url, options)
+		.then(async (res) => {
+			const blob = await res.blob()
+
+			const data = await new Response(blob).arrayBuffer()
+			const base64Data = btoa(new Uint8Array(data).reduce((data, byte) => data + String.fromCharCode(byte), ""))
+
+			return { blob, base64Data }
+		})
+		.catch((err) => {
+			console.error("Erreur lors de l'envoi de la requête:", err)
+			return undefined
+		})
+}
+
+// On envoie le ZPL à l'imprimante
+const printZpl = async ({ zpl }: { zpl?: string | undefined }) => {
+	if (!zpl) return console.error("ZPL is undefined")
+
+	const url = "http://192.168.1.60:8000/impression"
+
+	const options = {
+		method: "POST",
+		headers: {
+			"Content-Type": "text/plain",
+		},
+		body: zpl,
+	}
+
+	const ok = await fetch(url, options).then((res) => res.ok)
+
+	if (ok) alert("Impression envoyée avec succès")
+	else console.error("Erreur lors de l'impression")
 }
